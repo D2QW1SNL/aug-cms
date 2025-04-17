079ddc639b18dad1e035ef45529fa892c5f7f73b
summer <1005572910@qq.com>
2025-04-17 11:39:07 +0800

    提交SpringContextUtils工具类

---
+++ aug-cms-service/src/main/java/com/hl/aug/cms/service/SpringContextUtils.java
@@ -0,0 +1,50 @@
+
+
+package com.hl.aug.cms.service;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * Spring Context 工具类
+ */
+@Component
+public class SpringContextUtils implements ApplicationContextAware {
+	public static ApplicationContext applicationContext;
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext)
+			throws BeansException {
+		SpringContextUtils.applicationContext = applicationContext;
+	}
+
+	public static Object getBean(String name) {
+		return applicationContext.getBean(name);
+	}
+
+	public static <T> T getBean(final Class<T> type) {
+		return applicationContext.getBean(type);
+	}
+
+
+	public static <T> T getBean(String name, Class<T> requiredType) {
+		if (applicationContext == null) return null;
+
+		return applicationContext.getBean(name, requiredType);
+	}
+
+	public static boolean containsBean(String name) {
+		return applicationContext.containsBean(name);
+	}
+
+	public static boolean isSingleton(String name) {
+		return applicationContext.isSingleton(name);
+	}
+
+	public static Class<? extends Object> getType(String name) {
+		return applicationContext.getType(name);
+	}
+
+}
